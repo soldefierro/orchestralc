882c041c87f514c296cf811f83cf67511f3b8d92
Sol de Fierro <undisclosed>
2019-01-05 22:21:07 -0700

    initial work

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
+
+
+fn main() {
+    let matches = App::new("orchestralc")
+        .version("0.1.0")
+        .author("Sol de Fierro <undisclosed>")
+        .about("Runtime free template-to-rust compiler")
+        .subcommand(SubCommand::with_name("compile")
+            .about("compile an orchestralc template into a rust file")
+            .version("0.1.0")
+            .author("Sol de Fierro <undisclosed>")
+            .arg(Arg::with_name("INPUT")
+                .help("location of input file")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("OUTPUT")
+                .help("location of output file. will create; overwrite")
+                .required(true)
+                .index(2))
+            ).get_matches();
+
+
+}
---
+++ src/parameters.rs
@@ -0,0 +1,5 @@
+struct Parameter {
+    name: String, // name of parameter
+    type: String, // String of the reference to the type name
+    required: bool, // whether the parameter is needed in all cases. will be represented by an option
+}
